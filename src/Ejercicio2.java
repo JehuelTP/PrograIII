5398e8e634706ee9ac42cc00c0d736d583c3eb99
JehuelTP <162132210+JehuelTP@users.noreply.github.com>
2025-03-21 09:22:50 -0400

    Add files via upload
    21/03/25

---
+++ src/Ejercicio2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    String[] original;
+    String[] inverso;
+
+    Scanner scanner;
+
+    public Ejercicio2(){
+        original = new String[5];
+        inverso = new String[5];
+
+        scanner = new Scanner(System.in);
+    }
+    public void ejecutar(){
+        for (int i = 0; i < original.length; i++) {
+            System.out.println("original[" + i + "]: ");
+            String cadena = scanner.nextLine();
+
+            original[i] = cadena;
+        }
+        int indiceOriginal = 0;
+        int indiceInverso = 4;
+        while (indiceOriginal < original.length){
+            inverso[indiceInverso] = original[indiceOriginal];
+             indiceOriginal++;
+             indiceInverso--;
+        }
+        for (int i = 0; i < inverso.length; i++) {
+            System.out.println("inverso[" + i + "]: " + inverso[i]);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*Ejercicio1 ejercicio1 = new Ejercicio1();
+        ejercicio1.ejecutar();*/
+
+        /*Ejercicio2 ejercicio2 = new Ejercicio2();
+        ejercicio2.ejecutar();*/
+
+        Ejercicio3 ejercicio3 = new Ejercicio3();
+        ejercicio3.ejecutar();
+
+    }
+}
